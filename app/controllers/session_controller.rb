42a9715e7ab5b8d9a384cb233de70268f46d6130
kkosowsk <79844710+kkosowsk@users.noreply.github.com>
2024-06-14 13:15:35 +0200

    Update session_controller.rb
    add skip verify_authenticity_token

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -4,6 +4,7 @@ class SessionController < ApplicationController
   before_action :check_local_login_allowed,
                 only: %i[create forgot_password passkey_challenge passkey_login]
   before_action :rate_limit_login, only: %i[create email_login]
+  skip_before_action :verify_authenticity_token
   skip_before_action :redirect_to_login_if_required
   skip_before_action :preload_json,
                      :check_xhr,
